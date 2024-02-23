c745e3a219d26270efcc69280c582148b5f7daad
Ronal-Cabrera <116983660+Ronal-Cabrera@users.noreply.github.com>
2024-02-22 21:46:14 -0600

    datos de la BD

--- src/dbConfig.js
+++ src/dbConfig.js
@@ -1,4 +1,16 @@
 // dbconfig.js
+
+module.exports = {
+  user: 'db_aa50ce_rcabrerac4_admin',
+  password: 'rcabrerac4',
+  server: 'SQL5110.site4now.net',  
+  database: 'db_aa50ce_rcabrerac4',
+  options: {
+    encrypt: false, // Si estás utilizando Azure, establece esto en 'true'
+  },
+};
+
+/*
 module.exports = {
     user: 'rrrrr55555',
     password: 'rrrrr55555',
@@ -8,4 +20,4 @@ module.exports = {
       encrypt: false, // Si estás utilizando Azure, establece esto en 'true'
     },
   };
-  
+*/
